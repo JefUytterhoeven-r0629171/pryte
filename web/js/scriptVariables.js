555638f90cf3d0fb1c0c843f3c8d80967d50d94e
r0663029 <sam.janssens@student.ucll.be>
2020-02-14 15:49:55 +0100

    input and output variables form

---
+++ web/js/scriptVariables.js
@@ -0,0 +1,25 @@
+let tellerInput = 2;
+let tellerOutput = 2;
+function addInputVariable() {
+    var inputDiv = document.getElementById('inputDiv');
+    inputDiv.innerHTML += '<div id="'+ tellerInput +'"><label for="input' + tellerInput +'">Type input variable '+ tellerInput +'</label>: <input type="text" name="input" id="input' + tellerInput + '"/><br></div>';
+    tellerInput++;
+}
+function addOutputVariable() {
+    var outputDiv = document.getElementById('outputDiv');
+    outputDiv.innerHTML += '<div id="'+ tellerOutput +'"><label for="output' + tellerOutput + '">Type output variable ' + tellerOutput + '</label>: <input type="text" name="output" id="' + tellerOutput + '"/><br><button type="button" onclick="deleteOutputVar(tellerOutput)">- output var</button></div>';
+    tellerOutput++;
+}
+
+function deleteOutputVar(teller){
+    console.log(teller)
+    document.getElementById("outputDiv").removeChild(document.getElementById(teller))
+}
+
+function addToQueue(){
+
+}
+
+function deleteScript(){
+
+}
