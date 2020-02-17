61c8058cf3ab6edf4180e3795c9a0e054d25e144
JefUytterhoeven-r0629171 <32485244+JefUytterhoeven-r0629171@users.noreply.github.com>
2020-02-17 11:11:59 +0100

    python files can get executed

--- src/domain/RunPythonScript.java
+++ src/domain/RunPythonScript.java
@@ -1,5 +1,6 @@
 package domain;
 
+import javax.swing.filechooser.FileSystemView;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,22 +11,30 @@ public class RunPythonScript {
     Process mProcess;
 
     public void runScript(String path, String argumenten){
-        Process process;
-        try{
-            process = Runtime.getRuntime().exec(new String[]{path});
-            mProcess = process;
-        }catch(Exception e) {
-            System.out.println("Exception Raised" + e.toString());
-        }
-        InputStream stdout = mProcess.getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stdout,StandardCharsets.UTF_8));
-        String line;
-        try{
-            while((line = reader.readLine()) != null){
-                System.out.println("stdout: "+ line);
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        try {
+            // Run this on Windows, cmd, /c = terminate after this run
+            if(argumenten == null){
+                processBuilder.command("python", path );
+            }else{
+                processBuilder.command("python", path, argumenten );
             }
-        }catch(IOException e){
-            System.out.println("Exception in reading output"+ e.toString());
+
+            processBuilder.start();
+            Process process = processBuilder.start();
+
+            // blocked :(
+            BufferedReader nreader =
+                    new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String nline;
+            while ((nline = nreader.readLine()) != null) {
+                System.out.println(nline);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
