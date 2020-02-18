5690f590732405f07e9828c8e17eb4eb7d5c874b
JefUytterhoeven-r0629171 <32485244+JefUytterhoeven-r0629171@users.noreply.github.com>
2020-02-18 09:32:59 +0100

    het is mogelijk om dynamisch toegevoegde scripts aan de que toe te voegen.

--- web/js/scriptQueue.js
+++ web/js/scriptQueue.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-
+var scripts
 function getAllScripts() {
     console.log("de scripts worden opgeroepen");
     xhr.open("GET", "Controller?action=GetScripts&type=assync", true);
@@ -13,11 +13,11 @@ function getScripsLijst() {
     var table = document.getElementById("availablescriptstable");
     if (xhr.status == 200 && xhr.readyState == 4) {
         console.log("de scripts zijn aangekomen");
-        var scripts = JSON.parse(xhr.responseText);
+        scripts = JSON.parse(xhr.responseText);
         console.log(scripts);
         for(var i in scripts){
-            console.log(scripts[i].naam);
-           // table +=  ' <tr id="' + scripts[i].ID + '"> <td>'+ scripts[i].naam +'</td> <td>'+ scripts[i].extension +'</td> <td></td> <td></td><td><button type="button" onclick="addToQueue(' + scripts[i].ID + ')">Add to queue</button></td><td><button type="button" onclick="deleteScript()"> X </button></td> </tr>'
+            console.log(scripts[i].Id);
+            table.innerHTML +=  ' <tr id="' + scripts[i].naam + '"> <td>'+ scripts[i].naam +'</td> <td>'+ scripts[i].extension +'</td> <td></td> <td></td><td><button type="button" onclick="addToQueue( \'' + scripts[i].naam + '\')">Add to queue</button></td><td><button type="button" onclick="deleteScript()"> X </button></td> </tr>'
         }
     }
 }
